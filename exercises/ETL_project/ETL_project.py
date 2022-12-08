4367bfe9313ff27293766ae2c50a77306069e102
Wannes01 <wannes.hermans@edu.vlerick.com>
2022-12-08 15:45:53 +0000

    etl project

--- exercises/ETL_project/ETL_project.py
+++ exercises/ETL_project/ETL_project.py
@@ -2,7 +2,8 @@ from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
 BUCKET = "dmacademy-course-assets"
-KEY = "airflow/data.csv"
+KEY1 = "vlerick/after_release.csv"
+KEY2 = "vlerick/pre_release.csv"
 
 config = {
     "spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1",
@@ -11,5 +12,17 @@ config = {
 conf = SparkConf().setAll(config.items())
 spark = SparkSession.builder.config(conf=conf).getOrCreate()
 
-df = spark.read.csv(f"s3a://{BUCKET}/{KEY}", header=True)
-df.show()
+#1. Read the CSV data from this S3 bucket using PySpark
+df_after = spark.read.csv(f"s3a://{BUCKET}/{KEY1}", header=True)
+df_pre = spark.read.csv(f"s3a://{BUCKET}/{KEY2}", header=True)
+
+df_pre.show()
+df_after.show()
+
+#2. Convert the Spark DataFrames to Pandas DataFrames
+pre = df_pre.toPandas()
+after = df_after.toPandas()
+
+#3. Rerun the same ML training and scoring logic that you had created prior 
+#   to this class, starting with the Pandas DataFrames you got in step 2
+
